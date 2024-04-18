78be1a451ac9ea2eb2cfcb0c9c2fb8cbb96a19e4
Talia Pulsifer <taliapulsifer@outlook.com>
2024-04-18 00:07:55 -0500

    Adding put

--- capstone/backend/routes/bookRoutes.js
+++ capstone/backend/routes/bookRoutes.js
@@ -44,4 +44,42 @@ router.delete('/books/id', async (req, res) => {
     }
 });
 
+router.put('/books/:id', async (req, res) => {
+    try {
+        const bookID = req.params.id;
+        const {title, author, publisher, isbn} = req.body;
+
+        const updatedBook = await Book.findByIDAndUpdate
+        (
+            bookID,
+            {title, author, publisher, isbn},
+            {new: true}
+        );
+
+        res.json({message: 'Book updated successfully', book: updatedBook});
+
+    } catch (error) {
+        console.error('Error updating book: ', error);
+        res.status(500).json({ error: 'Internal server error'})
+    }
+});
+
+router.put('books/:id/checkout', async (req, res) => {
+    try {
+        const bookId = req.params.id;
+        const {checkedOutBy, dueDate} = req.body;
+
+        const updatedBook = await Book.findByIDAndUpdate(
+            bookId,
+            {status: 'checked out', checkedOutBy, dueDate},
+            {new: true}
+        );
+
+        res.json({message: 'Book checked out successfully', book: updatedBook});
+    } catch (error) {
+        console.error('Error updating book checkout status: ', error);
+        res.status(500).json({messgae: 'Internal server error'});
+    }
+});
+
 module.exports = router;
